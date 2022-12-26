1b86d98188b838f68a0fd106d2eee58f765998b1
Fredson Chaves <43495376+fredsonchaves07@users.noreply.github.com>
2022-12-26 18:09:27 -0300

    feat: initial project

---
+++ product/src/test/java/com/fredsonchaves07/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves07.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
