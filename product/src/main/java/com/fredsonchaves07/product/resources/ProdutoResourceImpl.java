fd7da6dd3838ae238f24fe13bfe03d116169a893
Fredson <fredsonchaves07@gmail.com>
2023-01-03 18:30:52 -0300

    feat: aplicando path

--- product/src/main/java/com/fredsonchaves07/product/resources/ProdutoResourceImpl.java
+++ product/src/main/java/com/fredsonchaves07/product/resources/ProdutoResourceImpl.java
@@ -1,13 +1,17 @@
 package com.fredsonchaves07.product.resources;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fredsonchaves07.product.dtos.ProdutoCriacaoDTO;
 import com.fredsonchaves07.product.dtos.ProdutoDTO;
+import com.fredsonchaves07.product.entities.Produto;
 import com.fredsonchaves07.product.services.ProdutoService;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.web.JsonPath;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,5 +30,14 @@ public class ProdutoResourceImpl implements ProdutoResource {
     @Override
     public ProdutoDTO buscarPorId(@PathVariable Long id) {
         return produtoService.buscarPorId(id);
+    }
+
+    @PatchMapping("{id}")
+    public ProdutoDTO update(@PathVariable Long id, @RequestBody JsonPatch patch) throws JsonPatchException, JsonProcessingException {
+        ProdutoDTO produtoDTO = produtoService.buscarPorId(id);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.convertValue(produtoDTO, JsonNode.class);
+        JsonNode patchJsonNode = patch.apply(jsonNode);
+        objectMapper.treeToValue(patchJsonNode, ProdutoDTO.class);
     }
 }
